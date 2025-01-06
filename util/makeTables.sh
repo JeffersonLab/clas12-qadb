f4d4a103c14cb393c3b8dc606aa6d903e3634071
Christopher Dilks <c-dilks@users.noreply.github.com>
2025-01-06 13:02:32 -0500

    feat: pre-commit hook

--- util/makeTables.sh
+++ util/makeTables.sh
@@ -8,12 +8,7 @@ source environ.sh
 
 pushd $QADB
 for file in $(find -P qadb -name "qaTree.json"); do
-  run-groovy util/parseQaTree.groovy $file
+  echo run-groovy util/parseQaTree.groovy $file # FIXME # FIXME # FIXME # FIXME # FIXME # FIXME # FIXME # FIXME # FIXME
   util/makeMiscTable.rb $file
 done
 popd
-
-git status --porcelain=v1 |\
-  grep -E '\.table$|miscTable\.md$' \
-  && { echo "generated files have changed" >&2; exit 1; } \
-  || exit 0
