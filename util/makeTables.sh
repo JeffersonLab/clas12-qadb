ccdd1e3bf1cdd04740970951626d1743c3ab7a60
Christopher Dilks <c-dilks@users.noreply.github.com>
2025-01-06 16:18:27 -0500

    test: just groovy?

--- util/makeTables.sh
+++ util/makeTables.sh
@@ -8,7 +8,7 @@ source environ.sh
 
 pushd $QADB
 for file in $(find -P qadb -name "qaTree.json"); do
-  # run-groovy util/parseQaTree.groovy $file
+  groovy util/parseQaTree.groovy $file
   util/makeMiscTable.rb $file
 done
 popd
